b108f3aae16ca2c523ac8a22e4a15e2e1e31f40d
lokesh <chokkalokesh0718@gmail.com>
2024-03-08 18:50:49 +0530

    2nd

--- axios/SECURE loginand Signup pages/models/tasks.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-
-
-const DetailSchema = new mongoose.Schema({
-    name:String,
-    password:String
-})
-
-module.exports = mongoose.model('data',DetailSchema)
